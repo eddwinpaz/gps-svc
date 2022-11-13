6dac1a6ef06a34f6edd4efe440acc6f0b300967e
eddwinpaz <pazeddwin@gmail.com>
2022-11-13 20:33:53 -0300

    addded new user pw db example

--- src/tcpHandler.ts
+++ src/tcpHandler.ts
@@ -1,4 +1,5 @@
 import net from "net";
+import { AppDataSource } from "./db/postgres";
 import { Location } from "./entities/Location";
 
 export const tcpHandler = (socket: net.Socket): void => {
@@ -15,13 +16,15 @@ export const tcpHandler = (socket: net.Socket): void => {
     if (data.toString().includes("+RESP")) {
       // socket.write("+SACK:GTFRI");
 
+      const repository = AppDataSource.getRepository(Location);
+
       const raw: string = data.toString();
       const imei: string = raw.split(",")[2];
       const location: Location = new Location();
       location.imei = imei;
       location.raw = raw;
 
-      await location.save();
+      await repository.save(location);
       console.log(data.toString());
     }
     // +BUFF:GTFRI,450102,865084030006793,,,10,1,5,3.5,0,592.8,-70.631011,-33.449598,20221112174035,0730,0001,32D5,5F15,00,0.0,,,,69,420000,,,,20221112174036,068F
