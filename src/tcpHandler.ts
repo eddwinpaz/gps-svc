f82b0387743428a390cde426d5b48f940e4e5dfc
eddwinpaz <pazeddwin@gmail.com>
2022-11-13 20:25:50 -0300

    addded new user pw db example

--- src/tcpHandler.ts
+++ src/tcpHandler.ts
@@ -2,33 +2,35 @@ import net from "net";
 import { Location } from "./entities/Location";
 
 export const tcpHandler = (socket: net.Socket): void => {
-    
-        socket.on("data", async (data: Buffer) => {
-        //   console.log(data.toString());
-          if(data.toString().includes("GTHBD")){
-            socket.write("+SACK:GTHBD");
-          }
+  socket.on("data", async (data: Buffer) => {
+    console.log("++++++++++++++++++++++++++");
+    console.log("data received");
+    console.log(data.toString());
+    console.log("++++++++++++++++++++++++++");
+    if (data.toString().includes("GTHBD")) {
+      socket.write("+SACK:GTHBD");
+    }
 
-          if(data.toString().includes("+BUFF:GTFRI")){
-            socket.write("+SACK:GTFRI");
+    if (data.toString().includes("+BUFF:GTFRI")) {
+      socket.write("+SACK:GTFRI");
 
-            const raw: string = data.toString();
-            const imei: string = raw.split(",")[2];
-            const location: Location = new Location();
-            location.imei = imei;
-            location.raw = raw;
+      const raw: string = data.toString();
+      const imei: string = raw.split(",")[2];
+      const location: Location = new Location();
+      location.imei = imei;
+      location.raw = raw;
 
-            await location.save();
-            console.log(data.toString());
-          }
-          // +BUFF:GTFRI,450102,865084030006793,,,10,1,5,3.5,0,592.8,-70.631011,-33.449598,20221112174035,0730,0001,32D5,5F15,00,0.0,,,,69,420000,,,,20221112174036,068F
-        });
-      
-        socket.on("end", () => {
-          console.log("Connection has been closed.");
-        });
-      
-        socket.on("close", () => {
-          console.log("Connection has been closed.");
-        });
-}
+      await location.save();
+      console.log(data.toString());
+    }
+    // +BUFF:GTFRI,450102,865084030006793,,,10,1,5,3.5,0,592.8,-70.631011,-33.449598,20221112174035,0730,0001,32D5,5F15,00,0.0,,,,69,420000,,,,20221112174036,068F
+  });
+
+  socket.on("end", () => {
+    console.log("Connection has been closed.");
+  });
+
+  socket.on("close", () => {
+    console.log("Connection has been closed.");
+  });
+};
